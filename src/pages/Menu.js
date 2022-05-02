bcdba7f3d0887af1b1344b938f7da71902634da2
Justice A. Acheampong <just.appiah@gmail.com>
2022-05-02 00:22:07 -0400

    changes to menu.js

--- src/pages/Menu.js
+++ src/pages/Menu.js
@@ -41,9 +41,9 @@ export default function Menu() {
                  
           <div className="row" >
             <div className="fill  btn-group" >
-                <button className="btn btn-secondary w-20 mb-3"  onClick={() =>setData(lists)} >All</button>  
-                <button className="btn btn-secondary w-25 mb-3" onClick={() => filterItems('Breakfast')}>Breakfast</button> 
-                <button className="btn btn-secondary w-20 mb-3" onClick={() => filterItems('Lunch')}>Lunch</button> 
+                <button className="btn btn-secondary w-30 mb-3"  onClick={() =>setData(lists)} >All</button>  
+                <button className="btn btn-secondary w-30 mb-3" onClick={() => filterItems('Breakfast')}>Breakfast</button> 
+                <button className="btn btn-secondary w-25 mb-3" onClick={() => filterItems('Lunch')}>Lunch</button> 
                 <button className="btn btn-secondary w-20 mb-3" onClick={() => filterItems('Dinner')}>Dinner</button>
                 <button className="btn btn-secondary w-25 mb-3" onClick={() => filterItems('Drinks')}>Drinks</button> 
                 <button className="btn btn-secondary w-25 mb-3" onClick={() => filterItems('Dessert')}>Dessert</button> 
@@ -68,9 +68,9 @@ export default function Menu() {
                         
                           {/* <a href={`/${food._id}`} ><img src={img} alt=" " className="image"/></a> */}
                  <div className="menu" >
-                    <div className="col-md-4 ">
+                    <div className="col-md-9 ">
                        <div className="row">
-                       <div className="col-md-4 mb-3">
+                       <div className="col-md-9 mb-3">
                         <div className="card" >
                             <a href={`/${food._id}`} ><img src={img} className="card-img-top" alt=" " /></a>
                            <div className="card-body">
