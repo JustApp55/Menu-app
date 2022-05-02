b1b8bb529b84895df7e9b2f0401fb13ec710fa00
Justice A. Acheampong <just.appiah@gmail.com>
2022-05-02 12:10:23 -0400

    changes to menu.js

--- src/pages/Data.js
+++ src/pages/Data.js
@@ -3,25 +3,25 @@ import React from 'react'
 
 export const lists = [
     {
-        // id: " 6269597ff9216742f5f8b763",
+       
         id: "1",
         title: 'Waffle-Wich',
         category: 'Breakfast',
         price: 12.50,
         img: 'https://canadify.com/wp-content/uploads/2019/08/Tim-Hortons-Brings-Back-Belgian-Waffle-Breakfast-Sandwich-678x381.jpg',
-        desc: 'The standard build features a pair of real Belgian Waffles sandwiching savoury sausage, a fluffy egg omelette, and processed cheese.'
+        desc: 'The build features a pair of waffles sandwiching savoury sausage, egg omelette...'
     },
     {
-        // id: "6269594cf9216742f5f8b761",
+       
         id: "2 ",
         title: 'SteVege',
         category: 'Lunch',
         price: 24.50,
         img: 'https://thumbs.dreamstime.com/b/japanese-hibachi-lunch-made-fried-rice-shrimp-steak-vegetables-close-up-table-vertical-top-view-plate-172491252.jpg',
-        desc: 'Finger licking tasty Japanese lunch made of fried rice, shrimp, steak and vegetables close-up'
+        desc: 'Finger licking tasty Japanese lunch made of fried rice, shrimp, steak and vegetables...'
     },
     {
-        // id: "626977533e71bb8942684dd1",
+       
         id: " 3",
         title: 'Grilled Filet Mignon',
         category: 'Dinner',
@@ -30,16 +30,16 @@ export const lists = [
         desc: 'Grilled Filet Mignon with delicious classic French Bearnaise and dipping steamed asparagus spears.'
     },
     {
-        // id: "62697c75c1b877061f841765",
+        
         id: "4 ",
         title: 'Lobster Thermidor',
         category: 'Dinner',
         price: 16.50,
         img: 'https://exquisite-taste-magazine.com/wp-content/uploads/2019/11/Arwana-signature-dish-Lobster-Thermidor.jpg',
-        desc: 'French dish of lobster meat cooked in a rich wine sauce, stuffed back into a lobster shell, and browned. It comes with a mixture of egg yolks and Brandy sauce'
+        desc: 'French dish of lobster meat cooked in a rich wine sauce, stuffed back into a lobster shell with egg yolks'
     },
     {
-        // id: "62698140c1b877061f841769",
+   
         id: "5 ",
         title: 'Wild Plum Tea',
         category: 'Dessert',
@@ -48,7 +48,7 @@ export const lists = [
         desc: 'A muffin with jam with almonds, more enticing than any of the lunch entrees'
     },
     {
-        // id: "626984fec1b877061f84176c",
+        
         id: "6 ",
         title: 'Pina Colada',
         category: 'Drinks',
@@ -57,7 +57,7 @@ export const lists = [
         desc: 'Hands down, the BEST Pina Colada. The flavor trifecta of pineapple, coconut and rum just can’t be beat when it comes to feeling and tasting that tropical vibe.'
     },
     {
-        // id: "6269598ff9216742f5f8b764",
+       
         id: "7",
         title: 'Platano Maduros',
         category: 'Dinner',
@@ -66,16 +66,16 @@ export const lists = [
         desc: "Platano maduros dish which comes with traditional Spanish rice and beans on the side"
     },
     {
-        // id: "62697e68c1b877061f841767",
+        
         id: "8",
-        title: 'Strawberry Panna Cotta',
+        title: 'Panna Cotta',
         category: 'Dessert',
         price: 12.25,
         img: 'https://www.abakingjourney.com/wp-content/uploads/2021/02/Strawberry-Panna-Cotta-Feature.jpg',
-        desc: "This Strawberry Panna Cotta with Strawberry Sauce is a deliciously light and creamy summer dessert"
+        desc: "This Panna Cotta with Strawberry Sauce is a deliciously light and creamy summer dessert"
     },
     {
-        // id: "62698235c1b877061f84176a",
+      
         id: " 9",
         title: 'Classic Omelet',
         category: 'Breakfast',
@@ -84,7 +84,7 @@ export const lists = [
         desc: "A benedict omelet with fresh spinach for a balance meal"
     },
     {
-        // id: "62698367c1b877061f84176b",
+        
         id: "10 ",
         title: 'Dumbo Pancake ',
         category: 'Breakfast',
@@ -93,25 +93,25 @@ export const lists = [
         desc: "A brioche French toast with blackberries, lemon curd, and fresh blackberry syrup"
     },
     {
-        // id: "626984fec1b877061f84176c",
+       
         id: "11 ",
         title: 'Pina Colada ',
         category: 'Drinks',
         price: 11.95,
         img: 'https://www.acouplecooks.com/wp-content/uploads/2020/11/Pina-Colada-056.jpg',
-        desc: "Hands down, the BEST Pina Colada. The flavor trifecta of pineapple, coconut and rum just can’t be beat when it comes to feeling and tasting that tropical vibe."
+        desc: "Hands down, the BEST Pina Colada. The flavor trifecta of pineapple, coconut and rum."
     },
     {
-        // id: "62698637c1b877061f84176d",
+       
         id: "12 ",
         title: 'Strawberry Soda',
         category: 'Drinks',
         price: 9.95,
         img: 'https://foodmeanderings.com/wp-content/uploads/2021/04/Strawberry-Italian-Soda-Recipe.jpg ',
-        desc: "Strawberry Italian soda, a refreshing non-alcoholic drink with whip cream on top waiting for you to own it."
+        desc: "Strawberry Italian soda, a refreshing non-alcoholic drink with whip cream on top."
     },
     {
-        // id: "62698749c1b877061f84176e",
+       
         id: "13 ",
         title: 'SunnyTail',
         category: 'Drinks',
@@ -120,7 +120,7 @@ export const lists = [
         desc: "A thirst-quenching simple rye and ginger cocktail."
     },
     {
-        // id: "626a0b6da4a591fd155508f4",
+        
         id: "14 ",
         title: 'Rotisserie Vebedi',
         category: 'Lunch',
@@ -129,24 +129,13 @@ export const lists = [
         desc: "Our famous, all-natural, rotisserie vegetable, bread and potato pot pie."
     },
     {
-        // id: " 626a0e44a4a591fd155508f5",
+        
         id: "15 ",
         title: 'Fillet of Hake',
         category: 'Lunch',
         price: 22.95,
         img: 'https://media-cdn.tripadvisor.com/media/photo-s/1a/c9/cc/29/fillet-of-hake.jpg',
-        desc: "Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+        desc: "Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt."
     },
 ]
 
-// export default function Data() {
-//     return (
-//         <>
-//             {products.map((product) => {
-//                 return(
-
-//                 )
-//             })}
-//         </>
-//     )
-// }
